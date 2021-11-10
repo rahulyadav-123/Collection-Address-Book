803820dca94cb8699f409012f42e070861b1276a
rahul yadav <ry219613@gmail.com>
2021-11-10 23:07:18 +0530

    find the duplicate entry while adding the person name

--- com/bridgelabz/addressbook/AddressBookMain.java
+++ com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,32 +1,25 @@
 package com.bridgelabz.addressbook;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Welcome to AddressBook Mangament System");
-		Scanner sc = new Scanner(System.in);
-		AddressBook addressBook = new AddressBook();
-		while (true) {
-			System.out.println("Enter \n 1 To add The contact \n 2 To edit the contact \n 3 To delete the contact \n 4 to exit");
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    addressBook.addContact();
-                    break;
-                case 2:
-                    addressBook.editContact();
-                    break;
-                case 3:
-                    addressBook.deleteContact();
-                    break;
-                case 4:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid input");
-                    continue;
-            }
-		}
-	}
+		Map<String, AddressBook> map = new HashMap<>();
+        System.out.println("Enter Number of address book you want to add");
+        int count = scan.nextInt();
+        int i = 0;
+        while (i < count) {
+            System.out.println("Enter book name");
+            String bookName = scan.next();
+            AddressBook addressBook = new AddressBook();
+            addressBook.choice();
+			map.put(bookName, addressBook);
+            i++;
+        }
+        System.out.println(map);
+    }
 }
