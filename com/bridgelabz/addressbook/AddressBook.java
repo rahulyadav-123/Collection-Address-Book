979501483d4e30e637f6f7bd5dd0a4cd7a36e9a6
rahul yadav <ry219613@gmail.com>
2021-11-10 22:44:41 +0530

    Added method to exsisting edit contact

--- com/bridgelabz/addressbook/AddressBook.java
+++ com/bridgelabz/addressbook/AddressBook.java
@@ -1,43 +1,71 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
+	List<ContactPerson> list = new ArrayList<ContactPerson>();
+	ContactPerson person = new ContactPerson();
+	Scanner sc = new Scanner(System.in);
 	
-		ContactPerson person = new ContactPerson();
-		
-		public void AddContact() {
-			Scanner sc = new Scanner(System.in);	
-			System.out.println("Enter First Name : ");
-			String firstName = sc.next();
-			System.out.println("Enter Last Name : ");
-			String lastName = sc.next();
-			System.out.println("Enter Address : ");
-			String address = sc.next();
-			System.out.println("Enter City : ");
-			String city = sc.next();
-			System.out.println("Enter State : ");
-			String state = sc.next();
-			System.out.println("Enter Zip : ");
-			int zip = sc.nextInt();
-			System.out.println("Enter Mobile Number : ");
-			long phoneNumber = sc.nextLong();
-			System.out.println("Enter Email : ");
-			String email = sc.next();
-			person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+	public void AddContact() {
+		System.out.println("Enter number of contacts you need to create :");
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			createContact();
 		}
+	}
+	
+	public void createContact() {
+		System.out.println("Enter First Name : ");
+		String firstName = sc.next();
+		System.out.println("Enter Last Name : ");
+		String lastName = sc.next();
+		System.out.println("Enter Address : ");
+		String address = sc.next();
+		System.out.println("Enter City : ");
+		String city = sc.next();
+		System.out.println("Enter State : ");
+		String state = sc.next();
+		System.out.println("Enter Zip : ");
+		int zip = sc.nextInt();
+		System.out.println("Enter Mobile Number : ");
+		long phoneNumber = sc.nextLong();
+		System.out.println("Enter Email : ");
+		String email = sc.next();
+		person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		list.add(person);
+		printContact();		
+	}
+	
+	public void printContact() {
+		System.out.println("Contact Details Are: ");
+		System.out.println("Name 	     : "  + person.getFirstName() + " " + person.getLastName() + "\n"
+                + "Address      : "  + person.getAddress() + "\n"
+                + "City         : "  + person.getCity() + "\n"
+                + "State        : "  + person.getState() + "\n"
+                + "ZipCode      : "  + person.getZip() + "\n"
+                + "MobileNumber : "  + person.getPhonenumber() + "\n"
+                + "EmailId      : "  + person.getEmail() + "\n" );
 		
-		
-		public static void main(String[] args) {
-			AddressBook addressBook = new AddressBook();
-			addressBook.AddContact();
-			System.out.println("Name 	     : "  + addressBook.person.getFirstName() + " " + addressBook.person.getLastName() + "\n"
-	                + "Address      : "  + addressBook.person.getAddress() + "\n"
-	                + "City         : "  + addressBook.person.getCity() + "\n"
-	                + "State        : "  + addressBook.person.getState() + "\n"
-	                + "ZipCode      : "  + addressBook.person.getZip() + "\n"
-	                + "MobileNumber : "  + addressBook.person.getPhonenumber() + "\n"
-	                + "EmailId      : "  + addressBook.person.getEmail() + "\n" );
+	}
+	
+	public void editContact() {
+		System.out.println("Enter the name to edit the contact: ");
+		String editName = sc.next();
+		for(int i = 0; i < list.size(); i++) {
+			String name = list.get(i).getlastName();
+			if(name.equalsIgnoreCase(editName)) {
+				createContact();			
+			}
 		}
+	}
+	
+	public static void main(String[] args) {
+		AddressBook addressBook = new AddressBook();
+		addressBook.AddContact();
+		addressBook.editContact();
+	}
 }
 	
