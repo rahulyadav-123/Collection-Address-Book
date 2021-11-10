a7b005c5ea9b7990cf1deabfaaa36d2283746822
rahul yadav <ry219613@gmail.com>
2021-11-10 22:30:22 +0530

    Added contact using oops concept

--- com/bridgelabz/addressbook/AddressBook.java
+++ com/bridgelabz/addressbook/AddressBook.java
@@ -1,23 +1,43 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book");
-		String firstName = "Rahul";
-		String lastName = "Yadav";
-		String address = "Near hanuman Temple";
-		String city = "pune";
-		String state = "Maharashtra";
-		int pinCode = 444001;
-		long mobileNumber = 883142763;
-		String emailId = "ry219613@gmail.com";
-		System.out.println("Name 	     : "  + firstName + " " + lastName + "\n"
-                + "Address      : "  + address + "\n"
-                + "City         : "  + city + "\n"
-                + "State        : "  + state + "\n"
-                + "ZipCode      : "  + pinCode + "\n"
-                + "MobileNumber : "  + mobileNumber + "\n"
-                + "EmailId      : "  + emailId + "\n" );
-	}
+	
+		ContactPerson person = new ContactPerson();
+		
+		public void AddContact() {
+			Scanner sc = new Scanner(System.in);	
+			System.out.println("Enter First Name : ");
+			String firstName = sc.next();
+			System.out.println("Enter Last Name : ");
+			String lastName = sc.next();
+			System.out.println("Enter Address : ");
+			String address = sc.next();
+			System.out.println("Enter City : ");
+			String city = sc.next();
+			System.out.println("Enter State : ");
+			String state = sc.next();
+			System.out.println("Enter Zip : ");
+			int zip = sc.nextInt();
+			System.out.println("Enter Mobile Number : ");
+			long phoneNumber = sc.nextLong();
+			System.out.println("Enter Email : ");
+			String email = sc.next();
+			person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		}
+		
+		
+		public static void main(String[] args) {
+			AddressBook addressBook = new AddressBook();
+			addressBook.AddContact();
+			System.out.println("Name 	     : "  + addressBook.person.getFirstName() + " " + addressBook.person.getLastName() + "\n"
+	                + "Address      : "  + addressBook.person.getAddress() + "\n"
+	                + "City         : "  + addressBook.person.getCity() + "\n"
+	                + "State        : "  + addressBook.person.getState() + "\n"
+	                + "ZipCode      : "  + addressBook.person.getZip() + "\n"
+	                + "MobileNumber : "  + addressBook.person.getPhonenumber() + "\n"
+	                + "EmailId      : "  + addressBook.person.getEmail() + "\n" );
+		}
 }
 	
